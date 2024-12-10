4b8c206de4e1e8aaa623ead8a82a7a62c937ffed
xmoonchild <gomezp.aina@gmail.com>
2024-12-10 16:02:03 +0100

    Models amb vistes runserver

--- backend/EmissionFactor/views.py
+++ backend/EmissionFactor/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def EmissionFactor(request):
+    return render(request, "EmissionFactorView.html")
--- backend/InstrumentCost/views.py
+++ backend/InstrumentCost/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def InstrumentCost(request):
+    return render(request, "InstrumentCostView.html")
